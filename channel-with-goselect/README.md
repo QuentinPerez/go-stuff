236cd4236342bbfff7272b92d640ffe930dc57ad
Quentin Perez <qperez@ocs.online.net>
2015-12-22 11:35:30 +0100

    goselect: benchmark with 5000 instead of 10000

--- channel-with-goselect/README.md
+++ channel-with-goselect/README.md
@@ -9,10 +9,10 @@ go test -bench='.' -benchmem -cover -cpu=1,2,3,4 ./...
 ?   	github.com/QuentinPerez/go-stuff/channel-with-goselect	[no test files]
 testing: warning: no tests to run
 PASS
-BenchmarkStart10000  	      20	  97719785 ns/op	 2825312 B/op	  112866 allocs/op
-BenchmarkStart10000-2	      20	  82234619 ns/op	 2885856 B/op	  113797 allocs/op
-BenchmarkStart10000-3	      20	  83770198 ns/op	 2843811 B/op	  113139 allocs/op
-BenchmarkStart10000-4	      20	  82114092 ns/op	 2844860 B/op	  113156 allocs/op
+BenchmarkStart5000  	      30	  43595542 ns/op	 1419512 B/op	   56541 allocs/op
+BenchmarkStart5000-2	      50	  39558259 ns/op	 1415775 B/op	   56476 allocs/op
+BenchmarkStart5000-3	      50	  34770516 ns/op	 1385317 B/op	   56000 allocs/op
+BenchmarkStart5000-4	      50	  36051506 ns/op	 1394619 B/op	   56146 allocs/op
 coverage: 100.0% of statements
-ok  	github.com/QuentinPerez/go-stuff/channel-with-goselect/goselect	8.962s
+ok  	github.com/QuentinPerez/go-stuff/channel-with-goselect/goselect	7.552s
 ```
--- channel-with-goselect/goselect/goselect_test.go
+++ channel-with-goselect/goselect/goselect_test.go
@@ -3,14 +3,15 @@ package goselect
 import "testing"
 
 // go test -bench='.' -benchmem -cover -cpu=1,2,3,4
+// ?   	github.com/QuentinPerez/go-stuff/channel-with-goselect	[no test files]
 // testing: warning: no tests to run
 // PASS
-// BenchmarkStart10000  	      20	  93723669 ns/op	 2809683 B/op	  112617 allocs/op
-// BenchmarkStart10000-2	      20	  86137894 ns/op	 2880550 B/op	  113718 allocs/op
-// BenchmarkStart10000-3	      20	  82514322 ns/op	 2897494 B/op	  113977 allocs/op
-// BenchmarkStart10000-4	      20	  79759312 ns/op	 2866640 B/op	  113501 allocs/op
+// BenchmarkStart5000  	      30	  43595542 ns/op	 1419512 B/op	   56541 allocs/op
+// BenchmarkStart5000-2	      50	  39558259 ns/op	 1415775 B/op	   56476 allocs/op
+// BenchmarkStart5000-3	      50	  34770516 ns/op	 1385317 B/op	   56000 allocs/op
+// BenchmarkStart5000-4	      50	  36051506 ns/op	 1394619 B/op	   56146 allocs/op
 // coverage: 100.0% of statements
-// ok  	github.com/QuentinPerez/go-stuff/channel-with-goselect/goselect	7.655s
+// ok  	github.com/QuentinPerez/go-stuff/channel-with-goselect/goselect	7.552s
 
 func BenchmarkStart5000(b *testing.B) {
 	for i := 0; i < b.N; i++ {
