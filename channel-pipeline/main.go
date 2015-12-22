51f3dc66d4a87931e36d74934d0072bc0fc5a4ce
Quentin Perez <qperez42@gmail.com>
2015-12-22 23:42:23 +0100

    channel-pipeline: add buffered method

--- channel-pipeline/main.go
+++ channel-pipeline/main.go
@@ -24,8 +24,24 @@ func square(input <-chan int) <-chan int {
 	return out
 }
 
+func genWithBuffer(nb int) <-chan int {
+	out := make(chan int, nb)
+
+	for i := 1; i <= nb; i++ {
+		out <- i
+	}
+	close(out)
+	return out
+}
+
 func main() {
 	for v := range square(gen(10)) {
+		fmt.Println(v)
+	}
+	fmt.Println()
+	fmt.Println("With buffered channel")
+	fmt.Println()
+	for v := range square(genWithBuffer(10)) {
 		fmt.Println(v)
 	}
 }
