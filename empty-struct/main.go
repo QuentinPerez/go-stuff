850f5bf03d2393f6d2c66c4481b2a4db1c08cf5a
Quentin Perez <qperez42@gmail.com>
2016-01-10 00:03:00 +0100

    empty: make stringer

--- empty-struct/main.go
+++ empty-struct/main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"unsafe"
 )
 
 type Empty struct{}
 
 func (e Empty) String() string {
-	return "Empty string"
+	val := reflect.Indirect(reflect.ValueOf(e))
+	return val.Type().String()
+}
+
+type MakeStringer struct {
+	Empty
 }
 
 func main() {
 	var e Empty
 	var tab [10]Empty
+	var m MakeStringer
 
 	fmt.Println("string():", e)
 	fmt.Println("empty == struct{}{}:", e == struct{}{})
 	fmt.Println("empty.sizeof:", unsafe.Sizeof(e))
 	fmt.Println("tab.sizeof:", unsafe.Sizeof(tab))
+	fmt.Println(m)
 }
