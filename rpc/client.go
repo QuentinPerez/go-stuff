889cfb70adc136478ef8691ee52f9757b8a90e48
Quentin Perez <qperez@ocs.online.net>
2016-02-04 17:46:24 +0100

    rpc: Add async method

--- rpc/client.go
+++ rpc/client.go
@@ -38,4 +38,27 @@ func main() {
 	fmt.Println("Receive at:", result.Middle, "diff S/E", result.Middle.Sub(result.Start))
 	fmt.Println("End     at:", end, "diff M/E", end.Sub(result.Middle))
 	fmt.Println("Total", end.Sub(result.Start), result.NB)
+
+	asyncCall := client.Go("Call.SlowCall", rpcdefinition.Args{time.Now()}, &result, nil)
+	fmt.Println()
+	fmt.Println("SlowCall Async:")
+	id := 0
+	for {
+		select {
+		case ret := <-asyncCall.Done:
+			end = time.Now()
+			if ret.Error != nil {
+				log.Fatalf("error in Call %v", ret.Error)
+			}
+			fmt.Println()
+			fmt.Println("Start   at:", result.Start)
+			fmt.Println("Receive at:", result.Middle, "diff S/E", result.Middle.Sub(result.Start))
+			fmt.Println("End     at:", end, "diff M/E", end.Sub(result.Middle))
+			fmt.Println("Total", end.Sub(result.Start), result.NB)
+			return
+		default:
+			fmt.Printf("\rI'm waiting.... %v", id)
+			id++
+		}
+	}
 }
